a122fe2b178a803ac49ef580c433c3d31b5c0e0c
AraaCraft <brun.margaux66@gmail.com>
2024-10-27 18:18:45 +0100

    Racket 1 movement

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,29 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "SFML works!");
 
+    //Balle
     sf::CircleShape circle;
     circle.setRadius(15);
     circle.setFillColor(sf::Color::Cyan);
     circle.setPosition(screenWidth/2, screenHeight/2);
 
+    //Raquette joueur 1
     sf::RectangleShape rectangle1;
     rectangle1.setSize (sf::Vector2f (widthRectangle, heightRectangle));
     rectangle1.setFillColor(sf::Color::White);
-    rectangle1.setPosition(750-(widthRectangle/2),0);
+    rectangle1.setPosition(screenWidth/2-(widthRectangle/2),0);
 
+    //Raquette joueur 2
     sf::RectangleShape rectangle2;
     rectangle2.setSize (sf::Vector2f (widthRectangle, heightRectangle));
     rectangle2.setFillColor(sf::Color::White);
-    rectangle2.setPosition(750-(widthRectangle/2),screenHeight-heightRectangle);
+    rectangle2.setPosition(screenWidth/2-(widthRectangle/2),screenHeight-heightRectangle);
+
+    //Horloge
+    sf::Clock clock;
+
+    //Vitesse raquette
+    float speedRacket = 400;
 
     while (window.isOpen())
     {
@@ -36,10 +45,30 @@ int main()
                 window.close();
         }
 
+        //deltaTime
+        float deltaTime = clock.restart().asSeconds();
+
+        // Logic loop
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            if (rectangle1.getPosition().x >= 0)
+            {
+            rectangle1.move(-speedRacket * deltaTime,0);
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            if (rectangle1.getPosition().x <= screenWidth-widthRectangle)
+            {
+            rectangle1.move(+speedRacket * deltaTime,0); 
+            }
+        }
+
+        // Draw loop
         window.clear();
-        window.draw(circle);
-        window.draw(rectangle1);
-        window.draw(rectangle2);
+        window.draw(circle);    //Affiche la balle
+        window.draw(rectangle1);    //Affiche la raquette 1
+        window.draw(rectangle2);    //Affiche la raquette 2
         window.display();
     }
 
